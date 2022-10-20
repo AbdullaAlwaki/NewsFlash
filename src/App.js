d338fde84414af53f95853c1934c1a77631396fe
Nigel Nix <nigel.nix@outlook.com>
2022-10-20 09:28:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import Context from "./component/Context";
-<<<<<<< HEAD
-import Navbar from "./pages/Navbar";
-import Footer from "./component/Footer";
 
-function App() {
-  return (<Context>
-      <Navbar/>
-=======
 import Navbarr from "./pages/Navbarr";
 import Footer from "./pages/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (<Context>
       <Navbarr/>
->>>>>>> main
+
       <Footer/>
      </Context>
      
